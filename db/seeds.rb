c19fb08115e78b2a64ef9589b575b8b3305c1963
Erica Bista <ericabista77@gmail.com>
2019-03-11 10:15:54 +1100

    implmented custom scope for portfolio item

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
 	Blog.create!(
 		title: "My Blog Post #{blog}",
-		body: "This is a sample for blog post."
+		body: "This is a sample for blog post.",
+    topic_id: Topic.last.id
 		)
 end
 
@@ -12,10 +19,20 @@ end
   )
 end
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My career service",
+    subtitle: "Ruby on rails",
+    body: "this is a sample.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "Angular JS",
     body: "this is a sample.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
