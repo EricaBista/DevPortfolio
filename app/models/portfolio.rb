1d0cc81076aadc762dded0a98b14f62a8f2e92da
Erica Bista <ericabista77@gmail.com>
2019-03-11 11:26:33 +1100

    integrated technology model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,14 +1,16 @@
 class Portfolio < ApplicationRecord
 	include Placeholder
+
+	after_initialize :set_defaults
+
+	has_many :technologies
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	scope :ruby_on_rails, -> { where(subtitle: 'Ruby on rails') }
 
 	def self.angular
 		where(subtitle: 'Angular JS')
 	end
-
-	scope :ruby_on_rails, -> { where(subtitle: 'Ruby on rails') }
-
-	after_initialize :set_defaults
 
 	def set_defaults
 		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
